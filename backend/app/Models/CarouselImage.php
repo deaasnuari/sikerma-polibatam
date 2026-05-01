4f8465a8dcd5eef22a17a07da6ccc43373343223
Syahnaz Dwi <syhnzdwi@gmail.com>
2026-05-01 17:09:30 +0700

    Menambahkan model dan migrasi carousel image

---
+++ backend/app/Models/CarouselImage.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CarouselImage extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'image_path',
+        'sort_order',
+        'is_active',
+    ];
+}
